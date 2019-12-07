236c1569aa5ae322d9c25088b7560f960a1e611e
Syajel <32969749+Syajel@users.noreply.github.com>
2019-12-07 18:18:01 +0200

    Update MainActivity.java
    added database integration

--- src/main/java/com/sourcey/materiallogindemo/MainActivity.java
+++ src/main/java/com/sourcey/materiallogindemo/MainActivity.java
@@ -5,16 +5,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
-
 public class MainActivity extends AppCompatActivity {
-
+    public static DatabaseHelper db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        db = new DatabaseHelper(this);
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
